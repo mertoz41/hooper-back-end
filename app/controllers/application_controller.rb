e1cccf902d645fc46be911bd9584898d6d6e0c2a
mert <mertozka41@gmail.com>
2022-10-25 14:55:57 -0400

    routes updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,11 +38,17 @@ class ApplicationController < ActionController::API
         render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
     end
 
+    def cors_preflight_check
+        if request.method == 'OPTIONS'
+          cors_set_access_control_headers
+          render text: '', content_type: 'text/plain'
+        end
+      end
     def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, PATCH, OPTIONS'
-        headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+        response.headers['Access-Control-Max-Age'] = '1728000'
     end
             
 
