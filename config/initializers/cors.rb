8ca973140da7d1874f7eed3c757c964411ebf4b0
mert <mertozka41@gmail.com>
2022-10-27 12:32:21 -0400

    https needed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://hooper-frontend.herokuapp.com'
+    origins 'https://hooper-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
